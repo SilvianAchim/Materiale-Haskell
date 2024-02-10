1377ea382d7dfb1b63fe10fb7b01333055ca56bc
DESKTOP-C7BOI8E\Silvian <silvianachim101@gamil.com>
2024-02-10 10:43:47 +0200

    no message

---
+++ Examen PF/PF2023-L01.hs
@@ -0,0 +1,20 @@
+import Data.List
+
+myInt = 31415926535897932384626433832795028841971693993751058209749445923
+
+double :: Integer -> Integer
+double x = x+x
+
+
+--maxim :: Integer -> Integer -> Integer
+maxim x y = if (x > y)
+               then x
+          else y
+
+max3 x y z = let
+             u = maxim x y
+             in (maxim  u z)
+
+test = sum [1..100]
+
+sumOfVals = sum [1..1000]
